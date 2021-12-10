41a0b68ce1c7b720916df4de1df4068d722af0bb
EdsonnOliveira <72781591+EdsonnOliveira@users.noreply.github.com>
2021-12-10 14:36:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Batista da Fé</title>
+        <?php include('PHP/meta.php') ?>
+        <link rel="stylesheet" type="text/css" href="CSS/Index.css">
+    </head>
+    <body>
+        <section class='preview' style="background-image: url('IMG/BG/bg1.jpg')">
+            <?php include('PHP/header.php'); ?>
+            <div class='center'>
+                <h4 class='txtWhite txtCenter txt100'>o justo viverá</h4>
+                <h1 class='txtWhite txtCenter txt300 txtCourgette'>Pela Fé</h1>
+            </div>
+        </section>
+        <section id='alerts'>
+            <article onclick="location. href='register.php?type=2'">
+                <img src="IMG/Alerts/habitos.jpeg">
+            </article>
+            <article>
+                <img src="IMG/Alerts/culto fe.png">
+            </article>
+            <article>
+                <img src="IMG/Alerts/ebd.png">
+            </article>
+            <article onclick="location. href='register.php?type=1'">
+                <img src="IMG/Alerts/culto domingo.png">
+            </article>
+        </section>
+        <h1 class='txtTitle' subText='Seja bem-vindo'>Seja bem-vindo</h1>
+        <section id='videoWelcome'>
+            <img src="IMG/Video/video1.png" alt="">
+        </section>
+        <section id='rowPhotos'>
+            <img src="IMG/Photos/1.png" alt="">
+            <img src="IMG/Photos/2.png" alt="">
+            <img src="IMG/Photos/3.png" alt="">
+            <img src="IMG/Photos/4.png" alt="">
+            <img src="IMG/Photos/5.png" alt="">
+            <img src="IMG/Photos/6.png" alt="">
+
+            <img src="IMG/Photos/7.png" alt="">
+            <img src="IMG/Photos/8.png" alt="">
+            <img src="IMG/Photos/9.png" alt="">
+            <img src="IMG/Photos/10.png" alt="">
+            <img src="IMG/Photos/11.png" alt="">
+            <img src="IMG/Photos/12.png" alt="">
+        </section>
+        <h1 class='txtTitle' subText='Nossa missão'>Nossa missão</h1>
+        <section id='mission'>
+            <h2 class='txtCenter txt300 txtBlack'>Mostrar ao mundo que Cristo é o Senhor não apenas com palavras, mas com ações. Tudo na base da Fé!</h2>
+            <a href="#" class='button btBlack btRound btBig txtWhite txtUpper txt500'>Conhecer +</a>
+        </section>
+        <section id='ministry'>
+            <div>
+                <h1 class='txtWhite txt200'>"Ensina a criança no caminho em que deve andar, e, ainda quando for velho, não se desviará dele." <b class='txt300'>- PV 22:6</b></h1>
+                <h2 class='txtWhite txt400'>Conheça este e outros ministérios</h2>
+                <a href="#" class='button btWhite btRound btBig txtBlack txtUpper txt500'>Conheça agora</a>
+            </div>
+        </section>
+        <h1 class='txtTitle' subText='Mensagens'>Mensagens</h1>
+        <section id='mensage'>
+            <div>
+                <article>
+                    <img src="IMG/Video/mensage1.png" alt="">
+                    <div>
+                        <div>
+                            <h1>Palavra do Dia</h1>
+                            <h2>Pr. Sivaldo Pereira</h2>
+                        </div>
+                        <div>
+                            <a href="#" class='button btRound btRed btMedium txtWhite txt300 txtUpper'>
+                                <img src="IMG/Custom/play.png" alt="Play">
+                                Assistir
+                            </a>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="IMG/Video/mensage1.png" alt="">
+                    <div>
+                        <div>
+                            <h1>Palavra do Dia</h1>
+                            <h2>Pr. Sivaldo Pereira</h2>
+                        </div>
+                        <div>
+                            <a href="#" class='button btRound btRed btMedium txtWhite txt300 txtUpper'>
+                                <img src="IMG/Custom/play.png" alt="Play">
+                                Assistir
+                            </a>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="IMG/Video/mensage1.png" alt="">
+                    <div>
+                        <div>
+                            <h1>Palavra do Dia</h1>
+                            <h2>Pr. Sivaldo Pereira</h2>
+                        </div>
+                        <div>
+                            <a href="#" class='button btRound btRed btMedium txtWhite txt300 txtUpper'>
+                                <img src="IMG/Custom/play.png" alt="Play">
+                                Assistir
+                            </a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div>
+                <a href="" class='button btRound btBlack btBigger txtWhite txt300'>Ver +</a>
+            </div>
+        </section>
+        <?php include('PHP/footer.php'); ?>
+    </body>
+</html>
